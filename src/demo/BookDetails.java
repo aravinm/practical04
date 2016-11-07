9f30810462670f234d693aba85dfa6ef7fb25109
Aravin <151642b@sit.nyp.edu.sg>
2016-11-07 15:15:48 +0800

    Initial commit

---
+++ src/demo/BookDetails.java
@@ -0,0 +1,17 @@
+package demo;
+
+/**
+ * Created by 151642b on 11/7/2016.
+ */
+public class BookDetails {
+
+    private String bookId = null;
+    private String title = null;
+    private String firstName = null;
+    private String surname = null;
+    private float price = 0.0F;
+    private boolean onSale = false;
+    private int year = 0;
+    private String description = null;
+    private int inventory = 0;
+}
